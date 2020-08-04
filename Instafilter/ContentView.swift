2232dc25c32eed88adf3077e78e2dfc897cf9911
Chris Eadie <chris.eadie@outlook.com>
2020-08-04 13:37:21 +0100

    Initial Commit

---
+++ Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Chris Eadie on 04/08/2020.
+//  Copyright © 2020 Chris Eadie Designs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
