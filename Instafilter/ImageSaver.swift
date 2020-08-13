df57252506de82d4a9db594fdd7348a2b31d5597
Chris Eadie <chris.eadie@outlook.com>
2020-08-13 17:49:38 +0100

    Add success and error handling

--- Instafilter/ImageSaver.swift
+++ Instafilter/ImageSaver.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
     func writeToPhotoAlbum(image: UIImage) {
-        UIImageWriteToSavedPhotosAlbum(image, self, #selector(save-Error), nil)
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
     }
     
     @objc func saveError(_ image:UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        // Save complete
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
     }
 }
